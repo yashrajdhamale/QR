5ed41c8a5345caf0e8b018280fd8af4cfb828cf5
YASHRAJ DHAMALE <134129335+YASHRAJ81@users.noreply.github.com>
2024-03-26 05:57:25 +0530

    afd

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let text = document.getElementById("text");
 let qrcode = document.getElementById("qr-code").children[0];
 let buttonCopy = document.getElementById("buttonCopy");
+let h6 = document.getElementById("h6");
 
 function generate() {
   qrcode.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text.value}`;
   text.value = "";
+  h6.style.display = "block";
 }
 
-async function  copy(){
-    const img = document.getElementById('QR-Code');
-    const blob = await fetch(img.src).then((response) => response.blob());
+async function copy() {
+  const img = document.getElementById("QR-Code");
+  const blob = await fetch(img.src).then((response) => response.blob());
 
-    try {
-        await navigator.clipboard.write([
-            new ClipboardItem({ 'image/png': blob })
-        ]);
-        alert('Image copied to clipboard!');
-    } catch (error) {
-        console.error('Error copying image:', error);
-    }
+  try {
+    await navigator.clipboard.write([new ClipboardItem({ "image/png": blob })]);
+    alert("Image copied to clipboard!");
+  } catch (error) {
+    console.error("Error copying image:", error);
+  }
 }
